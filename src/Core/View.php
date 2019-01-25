1c70d067a800d748f20e8af946f6f8e881933907
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-01-25 16:01:25 +0530

    Require Twig and Symfony's HTTPFoundation to inherit responding methods

--- src/Core/View.php
+++ src/Core/View.php
@@ -9,17 +9,36 @@
 namespace Greentea\Core;
 
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig_Environment;
+
 abstract class View
 {
     protected function respond(Response $response, Request $request, $html)
     {
         $response->setContent($html);
         $response->prepare($request);
+
         return $response;
     }
 
     protected function prepareService(Service $service)
     {
        $service->setServiceMode(Service::MODE_READ);
+    }
+
+    /**
+     * @param Twig_Environment $twig
+     * @param string $template
+     * @param array $params
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    protected function render(Twig_Environment $twig, string $template, $params = []) : string
+    {
+        return $twig->render($template, $params);
     }
 }
