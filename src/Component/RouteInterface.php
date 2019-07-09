8b5f11d3530b3671c3318dd3631ac486293efd36
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2019-07-09 21:45:33 +0530

    Remove irrelevant buildPaths
    Changed it to a VO instead of a class doing too much. Routers should simply return an implementation of RouteVOInterface

--- src/Component/RouteInterface.php
+++ src/Component/RouteInterface.php
@@ -8,10 +8,20 @@
 
 namespace Greentea\Component;
 
-
-interface RouteInterface
+/**
+ * Interface RouteVOInterface
+ * @package Greentea\Component
+ *
+ * A Route Value Object should be able to return the following:
+ *
+ * - Controller (To update the model layer using services)
+ * - View (To read the model layer using services and render it for the user)
+ * - Method (The method to be called based on the request type)
+ *
+ * Any implementation should use immutable value objects to extract only route information
+ */
+interface RouteVOInterface
 {
-    public function buildPaths(string $controllerNamespace, string $viewNamespace) : void;
     public function resolveController() : ?string;
     public function resolveView() : ?string;
     public function resolveMethod() : string;
